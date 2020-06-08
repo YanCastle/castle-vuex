648d6ced1738e4f8ea6219e4c19acde4cd17f392
hyy <472118643@qq.com>
2020-06-08 15:17:57 +0800

    新增获取用户信息字段

--- src/modules/UserLogin.ts
+++ src/modules/UserLogin.ts
@@ -1,10 +1,22 @@
-import { VuexModule, Module, Action } from "vuex-module-decorators";
+import { VuexModule, Module, Action, Mutation } from "vuex-module-decorators";
 import User from "@ctsy/api-sdk/dist/modules/User";
 
 @Module
 export default class UserLogin extends VuexModule {
+  UserInfo: User.LoginResult = new User.LoginResult();
+  @Mutation
+  SetUserInfo(d) {
+    this.UserInfo = d;
+  }
+
+  get info() {
+    return this.UserInfo;
+  }
+
   @Action({ rawError: true })
   async UserLogin(data: any) {
-    return await User.AuthApi.login(data.Account, data.PWD);
+    let rs = await User.AuthApi.login(data.Account, data.PWD);
+    this.context.commit("SetUserInfo", rs);
+    return rs;
   }
 }
